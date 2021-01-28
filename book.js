d2e3eac6a31306a9ae30b21c7d342c2bdb5984b4
Tobias Toland <github@thinktt.e4ward.com>
2021-01-28 01:14:26 -0600

    separate get all moves function

--- book.js
+++ book.js
@@ -5,7 +5,6 @@ async function getRandomBookMove(fen) {
   const book = new OpeningBook()
   const bookPath = process.cwd() + "/EarlyQueen.bin"
   const fs = require("fs")
-  // const fen = 'rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq - 0 1'
 
   const movePromise = new Promise(resolve => {
     book.on("loaded", () => {
@@ -25,11 +24,36 @@ async function getRandomBookMove(fen) {
 }
 
 async function testMove() {
-  // const move = await getRandomBookMove('rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq - 0 1')
-  const move = await getRandomBookMove('8/pp3p2/1k4p1/1r5p/7P/4RPK1/P7/8 b - - 0 1')
-  console.log(move)
+  const move = await getAllBookMoves('rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq - 0 1')
+  // const move = await getRandomBookMove('8/pp3p2/1k4p1/1r5p/7P/4RPK1/P7/8 b - - 0 1')
 }
 
+function listBookMoves(moves, fen) {
+  chess.load(fen)
+  moves = moves.sort((a, b) => (a._weight < b._weight) ? 1 : -1)
+  for (const move of moves) {
+    const piece = chess.get(move._algebraic_move.slice(0,2)).type
+    console.log(piece, move._algebraic_move.slice(2), move._weight)
+  }
+}
 
-// testMove()
+async function getAllBookMoves(fen) {
+  const book = new OpeningBook()
+  const bookPath = process.cwd() + "/EarlyQueen.bin"
+  const fs = require("fs")
+
+  const movePromise = new Promise(resolve => {
+    book.on("loaded", () => {
+      console.log("book loaded")
+      const moves = book.find(fen)
+      resolve(moves)
+    })
+  })
+  book.load_book(fs.createReadStream(bookPath))
+
+  return movePromise
+}
+
+testMove()
+
 module.exports =  { getRandomBookMove }
