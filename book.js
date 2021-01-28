7635b5b0e1fa8412452b706d2956db105f69a31e
Tobias Toland <github@thinktt.e4ward.com>
2021-01-28 01:23:01 -0600

    a little more cleanup refactoring

--- book.js
+++ book.js
@@ -1,31 +1,19 @@
+const { get } = require('http')
 const ChessTools = require('./chess-tools/index.js')
 const OpeningBook = ChessTools.OpeningBooks.Polyglot
 
 async function getRandomBookMove(fen) {
-  const book = new OpeningBook()
-  const bookPath = process.cwd() + "/EarlyQueen.bin"
-  const fs = require("fs")
-
-  const movePromise = new Promise(resolve => {
-    book.on("loaded", () => {
-      console.log("book loaded")
-      const entries = book.find(fen)
-      let move = ""
-      if (entries) {
-        move = entries[Math.floor(Math.random() * entries.length)]._algebraic_move
-      }
-      resolve(move)
-    })
-  })
-
-  book.load_book(fs.createReadStream(bookPath))
-
-  return movePromise
+  const bookMoves = await getAllBookMoves(fen)
+  if (!bookMoves) return ""
+  move = bookMoves[Math.floor(Math.random() * bookMoves.length)]._algebraic_move
+  return move
 }
 
 async function testMove() {
-  const move = await getAllBookMoves('rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq - 0 1')
+  // const move = await getAllBookMoves('rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq - 0 1')
+  const move = await getRandomBookMove('rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq - 0 1')
   // const move = await getRandomBookMove('8/pp3p2/1k4p1/1r5p/7P/4RPK1/P7/8 b - - 0 1')
+  console.log(move)
 }
 
 function listBookMoves(moves, fen) {
@@ -37,11 +25,11 @@ function listBookMoves(moves, fen) {
   }
 }
 
-async function getAllBookMoves(fen) {
-  const book = new OpeningBook()
-  const bookPath = process.cwd() + "/EarlyQueen.bin"
-  const fs = require("fs")
+const book = new OpeningBook()
+const bookPath = process.cwd() + "/EarlyQueen.bin"
+const fs = require("fs")
 
+async function getAllBookMoves(fen) {
   const movePromise = new Promise(resolve => {
     book.on("loaded", () => {
       console.log("book loaded")
