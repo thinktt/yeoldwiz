d830a3e8693c47d78b903feb555119bf0d6b8a29
Tobias Toland <github@thinktt.e4ward.com>
2021-02-10 08:35:12 -0600

    move engine to own module

---
+++ engine.js
@@ -0,0 +1,89 @@
+const ChessUtils = require("./bot-o-tron/src/utils/ChessUtils")
+const { exec } = require('child_process')
+const chalk = require('chalk')
+
+// Start and engine running as Josh 7
+async function getMove(moves) {
+
+  const chess = new ChessUtils()
+  chess.applyMoves(moves)
+  const turn = chess.turn()
+
+  // const cmd = 'C:/Users/Toby/code/yeoldwiz/InBetween.exe'
+  // const cmd = 'C:/Users/Toby/code/yeoldwiz/TheKing350noOpk.exe'
+  const cmd = '/mnt/c/Users/Toby/code/yeoldwiz-lnx/TheKing350noOpk.exe'
+  const child = exec(cmd)
+
+
+  child.on('close', function (code) {
+      console.log('egine exited ' + code);
+  });
+ 
+  const movePromise = new Promise(resolve => {
+    child.stdout.on('data', (data) => {
+      process.stdout.write(chalk.red(data.toString()))
+      if (data.toString().includes('move')) {
+        child.stdin.write('quit\n')
+        const move = data.toString().match(/move ([a-z][1-9][a-z][1-9]?.)/)[1]
+        resolve(move)
+      }
+    });
+  })
+
+  // const clockTime='0 3:20 0'
+  // const moveTime='20000'
+  const clockTime='0 6:40 0'
+  const moveTime='40000'
+
+
+  console.log(clockTime)
+  console.log(moveTime)
+  
+  child.stdin.write('xboard\n')
+  child.stdin.write('post\n')
+  child.stdin.write('new\n')
+  child.stdin.write(`level ${clockTime}\n`)
+  child.stdin.write('cm_parm opk=150308\n')
+  
+  
+  // Josh 7
+  child.stdin.write('cm_parm default\n')
+  child.stdin.write('cm_parm opp=83 opn=83 opb=94 opr=88 opq=92\n')
+  child.stdin.write('cm_parm myp=83 myn=83 myb=94 myr=88 myq=92\n')
+  child.stdin.write('cm_parm mycc=162 mymob=175 myks=93 mypp=137 mypw=100\n')
+  child.stdin.write('cm_parm opcc=162 opmob=175 opks=93 oppp=137 oppw=100\n')
+  child.stdin.write('cm_parm cfd=300 sop=30 avd=-45 rnd=0 sel=6 md=99\n')
+  child.stdin.write('cm_parm tts=16777216\n')
+  child.stdin.write('easy\n')
+  
+  //get's engine to move in about 10 seconds
+  
+  //prepare to take move list
+  child.stdin.write('force\n')
+  
+  console.log(moves)  
+  for (const move of moves) {
+    child.stdin.write(`${move}\n`)
+  }
+  
+  child.stdin.write(`time ${moveTime}\n`)
+  child.stdin.write(`otim ${moveTime}\n`)
+  if (turn == 'w') {
+    console.log('engine moving as white')
+    child.stdin.write('white\n')
+  } else {
+    console.log('engine moving a black')
+    child.stdin.write('black\n')
+  }
+  
+  child.stdin.write('go\n')
+
+  return movePromise
+}
+
+
+function isWhitesTurn(n) {
+  return n % 2 == 0;
+}
+
+module.exports = { getMove }
