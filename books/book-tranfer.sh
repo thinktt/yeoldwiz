b542f209dde8f897cf9d967deaee3f5faf364377
Tobias Toland <github@thinktt.e4ward.com>
2021-02-25 09:23:57 -0600

    rename leftover bin books to proper names

--- books/book-tranfer.sh
+++ books/book-tranfer.sh
@@ -5,6 +5,7 @@ declare -a books=("Bipto" "CaptureBook" "DangerDon" "Depth2" "Depth4" "Depth6" "
 for book in "${books[@]}"
 do 
   echo moving "zz$book.bin to $book.bin" 
+  mv zz$book.bin $book.bin
   # echo "Translating $book "
   # ./obk2bin.exe $book
 done
