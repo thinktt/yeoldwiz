c1b0ea273489889f4963931aa65f323265ef1f10
Tobias Toland <github@thinktt.e4ward.com>
2021-03-07 08:13:16 -0600

    moved things creates simpleout.exe to test with

--- enginemin.js
+++ enginemin.js
@@ -1,36 +1,51 @@
 const { exec } = require('child_process')
-const chalk = require('chalk')
+// const chalk = require('chalk')
 
-// start the chess engine process, using the proper engine command
-let cmd = process.cwd() + '/TheKing350noOpk.exe'
-const child = exec(cmd)
 
-// on close event stop the process
-child.on('close', function (code) {
-    console.log('egine exited ' + code);
-});
+let child
+function startCmd() {
+  // let cmd = process.cwd() + '/simpleout.exe'
+  let cmd = process.cwd() + '/TheKing350noOpk.exe'
+  child = exec(cmd) 
+}
 
-// create a movePromise, when engine responds with a move it resolves and
-// sends an exit command to the engine, closing the process
-const movePromise = new Promise(resolve => {
-  child.stdout.on('data', (data) => {
-    process.stdout.write(chalk.red(data.toString()))
-    if (data.toString().includes('move')) {
-      child.stdin.write('quit\n')
-      const move = data.toString().match(/move ([a-z][1-9][a-z][1-9]?.)/)[1]
-      resolve(move)
-    }
-  });
-})
+try {
+  startCmd()
+} catch (error) {
+  console.log("looks like me made it")
+  // console.log(error)
+}
 
-// log on process errors
-child.stderr.on('data', (data) => {
-  process.stdout.write(chalk.red(data.toString()))
-})
 
-child.stdin.write('xboard\n')
-child.stdin.write('post\n')
-child.stdin.write(`time 2000\n`)
-child.stdin.write(`otim 2000\n`)
-child.stdin.write('go\n')
+
+
+
+// // on close event stop the process
+// child.on('close', function (code) {
+//     console.log('egine exited ' + code);
+// });
+
+// // create a movePromise, when engine responds with a move it resolves and
+// // sends an exit command to the engine, closing the process
+// const movePromise = new Promise(resolve => {
+//   child.stdout.on('data', (data) => {
+//     process.stdout.write(data.toString())
+//     if (data.toString().includes('move')) {
+//       child.stdin.write('quit\n')
+//       const move = data.toString().match(/move ([a-z][1-9][a-z][1-9]?.)/)[1]
+//       resolve(move)
+//     }
+//   });
+// })
+
+// // log on process errors
+// child.stderr.on('data', (data) => {
+//   process.stdout.write(data.toString())
+// })
+
+// child.stdin.write('xboard\n')
+// child.stdin.write('post\n')
+// child.stdin.write(`time 2000\n`)
+// child.stdin.write(`otim 2000\n`)
+// child.stdin.write('go\n')
 
---
+++ main2.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	// "bufio"
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Howdy")
+}
+
+
+
+// outStr, errStr := string(stdoutBuf.Bytes()), string(stderrBuf.Bytes())
+// fmt.Printf("\nout:\n%s\nerr:\n%s\n", outStr, errStr)
+
+	// for {
+  //   line, _, _ := stdOutBuf.ReadLine()
+	// 	if string(line) == "move e2e4" {
+	// 		os.Exit(0)
+	// 	}
+  //   fmt.Println(string(line))
+  // }
