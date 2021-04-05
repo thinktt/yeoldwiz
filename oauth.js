2e6a8ca8c56cd1d0d477ae6d91f8a9cb9d95bab8
Tobias Toland <github@thinktt.e4ward.com>
2021-04-05 00:18:40 -0500

    simple express server to proxy lichess tokens

---
+++ oauth.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const fetch = require('node-fetch');
+
+const app = express();
+const port = 5000;
+
+app.get('/token', async (req, res) => {
+  const query =  `grant_type=authorization_code&code=${req.query.code}&redirect_uri=http://localhost:8080`
+  fetch('https://oauth.lichess.org/oauth', {
+    body: query,
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Authorization': 'Basic TDQ3VHFwWm43aWFKcHBHTTpDZGY3aGxhSndKbWVyd2JESEZ1cWxQQVVnR1U3eGtTNw==',
+    },
+  })
+  .then(lires => {
+    if (!lires.ok) {
+      res.status(500).json({ error: "boo" });
+      throw new Error(res.statusText);
+    } 
+    return lires.json()
+  })
+  .then(data => {
+     res.json(data)
+  })
+  .catch(err => {
+    console.log(err)
+  })
+ 
+
+});
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
