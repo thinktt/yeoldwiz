598b5b2bfa3089393ff0385620fba37c7d50cda5
Tobias Toland <github@thinktt.e4ward.com>
2022-05-05 15:46:48 -0500

    wip game api for storing wiz opponents

---
+++ yowapi/main.js
@@ -0,0 +1,194 @@
+
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000
+const { MongoClient } = require("mongodb")
+const uri = "mongodb://localhost:27017/?maxPoolSize=20&w=majority"
+const client = new MongoClient(uri)
+const Ajv = require("ajv")
+const ajv = new Ajv()
+
+const gameSchema = {
+  type: "object",
+  properties: {
+    id: {type: "string", pattern: "^[a-zA-Z0-9]{8}$" },
+    opponent: {
+      type: "string", 
+      // enum: ["Marius", "Orin"],
+      pattern: "^[a-zA-Z0-9]*$",
+    }
+  },
+  required: ["id", "opponent"],
+  additionalProperties: false
+}
+
+app.use(express.json())
+
+app.post('/games', async (req, res) => {
+  // console.log(req.body)
+  const valid = ajv.validate(gameSchema, req.body)
+  if (!valid) {
+    res.status(400)
+    res.json(ajv.errors[0])
+    return
+  }
+  create(game).catch((err)=>{
+    res.status(500)
+
+  })
+  res.json({status: 'OK'})
+})
+
+// catch all error handler
+app.use((err, req, res, next) => {
+  if (err.statusCode) res.status(err.statusCode)
+    else res.statusCode(500)
+  res.json({ error: err.toString() })
+})
+
+
+// Connect and ping the Mongodb sever, then disconect, throw any errors
+async function run() {
+  try {
+    await client.connect()
+    await client.db("admin").command({ ping: 1 })
+    console.log("Connected successfully to server")
+  } finally {
+    await client.close()
+  }
+}
+// run().catch(console.dir)
+
+async function create(game) {
+  try {
+    // Connect the client to the server
+    await client.connect()
+    
+    // create the entry if it doesn't exist already
+    const res = await client.db("yow").collection('games').updateOne(  
+      {id : game.id}, 
+      {$setOnInsert: game},
+      {upsert: true},
+    )
+
+    console.log(res)
+    console.log("Creation successfull")
+  
+  } catch (err) {
+    return err
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close()
+  }
+}
+// create('duperduperd').catch(console.dir)
+
+// app.listen(port, () => console.log(`Server started on port ${port}`))
+
+// allows you to safely call an asycn function and easily detect trown errors
+// and respond to them with a Golang style if error statement
+async function safeCall(promise) {
+ try {
+   const res = await promise
+   return [res, null]
+ } catch (err) {
+   return [null, err]
+ }
+}
+
+async function throwErr() {
+  throw Error('Bad stuff')
+  return 'Howdy!'
+}
+
+async function main() {
+  let [a, err] = await safeCall(throwErr())
+  if (err) {
+    console.log(`There was an error: ${err.message}`)
+    return
+  }
+  console.log(a)
+}
+
+main()
+
+
+// const fetch = require('node-fetch')
+// const request = require('request')
+
+// const express = require('express')
+
+// const fetch = require('node-fetch')
+// const request = require('request')
+
+// const app = express()
+// const port = process.env.PORT || 5000
+
+
+// app.get('/health', (req, res) => {
+//   res.json({'status': 'ok'})
+// })
+
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type, Accept,Authorization,Origin")
+//   res.setHeader("Access-Control-Allow-Origin", "*")
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE")
+//   res.setHeader("Access-Control-Allow-Credentials", true)
+//   next()
+// })
+
+// app.get('/token', async (req, res) => {
+//   const auth = 'TDQ3VHFwWm43aWFKcHBHTTpDZGY3aGxhSndKbWVyd2JESEZ1cWxQQVVnR1U3eGtTNw=='
+  
+//   console.log('Received a code at ' + req.headers.host)
+//   console.log('Redirect url is set to ' + req.query.redirect_uri)
+
+//   const query =  `grant_type=authorization_code&code=${req.query.code}&redirect_uri=${req.query.redirect_uri}`
+//   // console.log(query)
+//   // console.log(auth)
+
+//   fetch('https://oauth.lichess.org/oauth', {
+//     body: query,
+//     method: 'POST',
+//     headers: {
+//       'Accept': 'application/json',
+//       'Content-Type': 'application/x-www-form-urlencoded',
+//       'Authorization': `Basic ${auth}` ,
+//     },
+//   })
+//   .then(lires => {
+//     if (!lires.ok) {
+//       // console.log(lires)
+//       res.status(400).json({ error: lires.statusText });
+//       throw new Error(res.statusText);
+//     } 
+//     return lires.json()
+//   })
+//   .then(data => {
+//      res.json(data)
+//   })
+//   .catch(err => {
+//     console.log(err)
+//     res.status(400).send('Error getting token')
+//   })
+
+// });
+
+// // app.get('/games/:gameId', (req, res) => {
+// //   fetch(`https://lichess.org/${req.params.gameId}`)
+// //   .then(lires => lires.text())
+// //   .then(data => {
+// //     res.send(data)
+// //   })
+// //   .catch(err => {
+// //     console.log(err)
+// //     res.status(400).send("Error getting the game")
+// //   })
+// // })
+
+// app.get('/games/:gameId', (req, res) => {
+//   req.pipe(request("https://lichess.org/" + req.params.gameId)).pipe(res);
+// })
+
+
+
