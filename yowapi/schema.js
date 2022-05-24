e8b475f27e7b23533dcfbce21374193aa5b8785a
Tobias Toland <github@thinktt.e4ward.com>
2022-05-24 14:22:09 -0500

    add user to game schema

--- yowapi/schema.js
+++ yowapi/schema.js
@@ -2,13 +2,14 @@ const game = {
   type: "object",
   properties: {
     id: {type: "string", pattern: "^[a-zA-Z0-9]{8}$" },
+    user: { type: "string"},
     opponent: {
       type: "string", 
       // enum: ["Marius", "Orin"],
       pattern: "^[a-zA-Z0-9]*$",
-    }
+    },
   },
-  required: ["id", "opponent"],
+  required: ["id", "user", "opponent"],
   additionalProperties: false
 }
 
