6c037a169f468c90a14c0087e8549a1147ea4a01
Tobias Toland <github@thinktt.e4ward.com>
2023-06-13 12:20:46 -0500

    runbook should be working as expected now

--- yowbot/src/book2.js
+++ yowbot/src/book2.js
@@ -1,16 +1,6 @@
 const ChessTools = require('chess-tools')
 const OpeningBook = ChessTools.OpeningBooks.Polyglot
 const fs = require("fs")
-// const cmps = require('./personalities.json')
-// const chess = new require('chess.js').Chess()
-// const { get } = require('http')
-// const { rejects } = require('assert')
-// const { channel } = require('diagnostics_channel')
-
-// load all the books when the module is loaded
-// let booksAreLoaded = false
-// const bookShelf = {}
-// loadAllBooks()
 
 module.exports =  { getHeavyMove , getRandomMove}
 
@@ -24,7 +14,12 @@ async function getRandomMove(fen, book) {
 
 // get's random book move favoring higher weights and ignoring zero weights
 async function getHeavyMove(fen, book) {
-  let bookMoves = await getAllBookMoves(fen, book)
+  let err = null
+  let bookMoves = await getAllBookMoves(fen, book).catch(e => err = e)
+  if (err) {
+    console.error(`error getting book moves: ${err.message}`)
+    process.exit(1)
+  }
   if (!bookMoves) return ""
 
   // sort moves by weight for easier viewing when logging the final array
@@ -37,9 +32,6 @@ async function getHeavyMove(fen, book) {
       weightedMoves.push(move._algebraic_move) 
     } 
   }
-  
-  // listBookMoves(bookMoves, fen)
-  // console.log(weightedMoves)
   
   // pick a random move from the weight distributed list
   move = weightedMoves[Math.floor(Math.random() * weightedMoves.length)]
@@ -59,96 +51,18 @@ async function getAllBookMoves(fen, bookName) {
         moves = book.find(fen)
       } catch (err) {
         console.error(`Failed to get book moves: ${err.message}`)
+        // reject(err)
       }
       resolve(moves)
     })
   })
-  book.load_book(fs.createReadStream(bookPath))
+  
+  const bookStream = fs.createReadStream(bookPath)
+  bookStream.on("error", (err) => {
+    console.error(`Failed to get book: ${err.message}`)
+    process.exit(1)
+  })
+  book.load_book(bookStream)
   return movePromise
 }
 
-
-// async function testMove() {
-//   // const move = await getAllBookMoves('rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq - 0 1')
-//   // const move = await getRandomBookMove('rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq - 0 1')
-//   const move = await getWeightFavoredBookMove('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1')
-//   // const move = await getRandomBookMove('8/pp3p2/1k4p1/1r5p/7P/4RPK1/P7/8 b - - 0 1')
-//   console.log(move)
-// }
-
-// function listBookMoves(moves, fen) {
-//   chess.load(fen)
-//   moves = moves.sort((a, b) => (a._weight < b._weight) ? 1 : -1)
-//   for (const move of moves) {
-//     const piece = chess.get(move._algebraic_move.slice(0,2)).type
-//     console.log(piece, move._algebraic_move.slice(2), move._weight)
-//   }
-// }
-
-
-
-
-// experiment with loading all the books at once and keeping them in mem
-// async function getAllBookMovesExp(fen, bookName) {
-//   if (!booksAreLoaded) {
-//     console.log('still waiting for books to load')
-//     await whenBooksareLoaded()
-//   }
-//   const book = bookShelf[bookName] 
-//   const moves = book.find(fen)
-//   return moves
-// }
-
-// async function loadAllBooks() {
-//   console.log('Loading all opening books in the background')
-//   for (const name in cmps) {
-//     const bookName = cmps[name].book
-    
-//     if (!bookShelf[bookName]) {
-//       // console.log(bookName)
-//       try {
-//         bookShelf[bookName] = await loadBook(bookName)
-//       } catch (err) {
-//         console.error(err.message)
-//       }
-//     }
-//   }
-
-//   // const used = process.memoryUsage().heapUsed / 1024 / 1024
-//   // console.log(`${Math.round(used * 100) / 100} MB`)
-
-//   booksAreLoaded = true
-//   console.log(`${Object.keys(bookShelf).length} books loaded`)
-// }
-
-// async function whenBooksareLoaded() {
-//   while(!booksAreLoaded) {
-//     await new Promise(r => setTimeout(r, 250))
-//   }
-// }
-
-// async function loadBook(bookName) {
-//   const book = new OpeningBook()
-//   const bookPath = process.cwd() + `/books/${bookName}`
-
-//   const bookPromise = new Promise((resolve, reject) => {
-//     const fileStream = fs.createReadStream(bookPath)
-//     fileStream.on('error', (err) => {
-//       console.log(err.message) 
-//       resolve(null) 
-//     }) 
-//     // resolve(fileStream)
-//     book.load_book(fs.createReadStream(bookPath))
-//     book.on("loaded", () => resolve(book))
-//   })
-
-//   return bookPromise
-// }
-
-
-// bad state test when book crashes app
-// const badFen = 'r2q1rk1/pbp1p1bp/1p4pn/3pPp2/3P4/N1Q4P/PPP1NPP1/R1B1K2R w KQ f6 0 12'
-// const badBook = 'Depth6.bin'
-// getHeavyMove(badFen, badBook).then((moves) => {
-//   console.log(moves)
-// })
--- yowbot/src/runbook.js
+++ yowbot/src/runbook.js
@@ -1,51 +1,24 @@
 const chessTools = require("./chessTools.js")
-const chalk = require('chalk')
 const book = require('./book2')
 
 const jsonStr = process.argv[2]
-console.log(`Received input: ${jsonStr}`)
+// console.log(`Received input: ${jsonStr}`)
 
 let input
 try {
   input = JSON.parse(jsonStr)
 } catch (e) {
-  console.log(`Error parsing input: ${e}`)
+  console.error(`error parsing json input: ${e}`)
   process.exit(1)
 }
 
-// const moves = []
-// const bookName = "EarlyQueen.bin"
 const moves = input.moves
 const bookName = input.book
 
-// const moves = [ 'd2d4', 'c7c5' ]
-// const moves = [ 'd2d4', 'c7c5', 'c2c4', 'c5d4' ]
-// const wizPlayer = 'Josh7'
-// getABunchOfMoves()
-
 getNextMove(moves, bookName)
 
-async function getABunchOfMoves() {
-  const bookMoves = {}
-  for (let i = 0; i<1000; i++) {
-    let bookMove = await getNextMove(moves, 'JW7', 'xyzzy')
-
-    if (bookMove === undefined) bookMove = {move :'none'}
-  
-    if (!bookMoves[bookMove.move]) {
-      bookMoves[bookMove.move] = 1
-    } else {
-      bookMoves[bookMove.move]++
-    }
-  
-  }
-  console.log(bookMoves)
-}
-
-
 async function getNextMove(moves, bookName) {
-      
-  console.log(chalk.blue(`Getting move from ${bookName}`))
+  // console.log(`Getting move from ${bookName}`)
 
   const chess = chessTools.create()
   chess.applyMoves(moves)
@@ -53,16 +26,23 @@ async function getNextMove(moves, bookName) {
   
   // if there are no legal moves then return
   if (!legalMoves.length) {
-    console.log("No legal moves")
-    return null
+    console.error("no legal moves")
+    process.exit(1)
   }
 
-  const bookMove = await book.getHeavyMove(chess.fen(), bookName)
+  let err = null 
+  const bookMove = await book.getHeavyMove(chess.fen(), bookName).catch(e => err = e)
+  if (err) {
+    console.error(`error getting book move: ${err.message}`)
+    process.exit(1)
+  }
+
   if (bookMove == "") {
-    console.log("no book move found")
-    return null
+    console.error("no book move")
+    process.exit(1) 
   }
 
-  console.log(`bookMove: ${bookMove}`)
-  return {move: bookMove, willAcceptDraw: false}
+  console.log(`${bookMove}`)
+  process.exit(0)
 }
+
