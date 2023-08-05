2db3d4cb6970aa2698dd512b4e221f6693ea2fc7
Tobias Toland <github@thinktt.e4ward.com>
2023-08-04 22:08:53 -0500

    goodbye to all cm assets, now pulled in

--- yowdeps/src-old/engineApi.js
+++
@@ -1,5 +0,0 @@
-const host = 'https://yowking.localhost'
-let yowKingToken = process.env.YOWKING_TOKEN
-
-async function getMove(gameId, moves, clockTimes) {
-  
--- yowdeps/src-old/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/thinktt/yeoldwiz
-
-go 1.20
