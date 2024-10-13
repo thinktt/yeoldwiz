eca417e58c355d7511d7b1b9dc2b0500e377a673
Tobias Toland <github@thinktt.e4ward.com>
2024-10-12 21:20:00 -0500

    store lichess games found db as lichessGames

--- yowdb/tranfer.mjs
+++ yowdb/tranfer.mjs
@@ -4,8 +4,8 @@ import { writeFileSync, readFileSync } from 'fs'
 
 
 
-const yowGames = await getGames(100)
 // const yowGames = JSON.parse(readFileSync('missingGames.json'))
+const yowGames = await getGames(100)
 
 const gameIds = []
 const yowGameMap = {}
@@ -24,6 +24,11 @@ const missingGames = findMissingGameIds(gameIds, games, yowGameMap)
 console.log(`storing ${missingGames.length} yow game in missingGames.json`)
 
 writeFileSync('missingGames.json', JSON.stringify(missingGames, null, 2))
+await insertLichessGames(games)
+const total = await getTotalLichessGames() 
+console.log(total, " lichess games stored in db")
+
+
 
 
 
@@ -77,6 +82,54 @@ function findMissingGameIds(gameIds, games, yowGameMap) {
   return missingGames
 }
 
+
+async function insertLichessGames(games) {
+  const uri = "mongodb://localhost:27017"
+  const client = new MongoClient(uri)
+  let err
+
+  await client.connect().catch(e => {
+    console.error("error connecting client: ", e)
+    process.exit(1)
+  })
+
+  const collection = client.db('yow').collection('lichessGames')
+  await collection.insertMany(games).catch(e => {
+    console.error("error inserting games: ", e)
+    process.exit(1)
+  })
+
+  await client.close().catch(e => {
+    console.error("error closing client: ", e)
+    process.exit(1)
+  })
+}
+
+
+async function getTotalLichessGames() {
+  const uri = "mongodb://localhost:27017"
+  const client = new MongoClient(uri)
+  
+  await client.connect().catch(e => {
+    console.error("error connecting client: ", e)
+    process.exit(1)
+  })
+  
+  const collection = client.db('yow').collection('lichessGames')
+  const count = await collection.countDocuments().catch(e => {
+    console.error("error counting games: ", e)
+    process.exit(1)
+  })
+  
+  await client.close().catch(e => {
+    console.error("error closing client: ", e)
+    process.exit(1)
+  })
+  
+  return count
+
+
+}
 async function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms))
 }
